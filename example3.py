521b78cd7b119e53257978bf7467487d384a72bc
Vedic Mukherjee <99448454+VedicAM@users.noreply.github.com>
2024-07-24 15:23:12 -0400

    Deleted example3.py

--- example3.py
+++
@@ -1,8 +0,0 @@
-import os
-from developer import Developer
-developer = Developer()
-
-for filename in os.listdir('exampleDir'):
-    f = os.path.join('exampleDir', filename)
-    print(f)
-    developer.write(f)
